b42338bf5b6745c6b8e236222593df2a8a7f47b9
Angel Motta <angelmotta@gmail.com>
2019-10-15 12:15:00 -0500

    UVA test PASSEDgit status Backtracking

--- p524_primeRingProblem_angelmotta.cpp
+++ p524_primeRingProblem_angelmotta.cpp
@@ -1,8 +1,11 @@
-#include <cstdio>     // std::cout
-#include <algorithm>    // std::next_permutation, std::sort
-#include <vector>
+#include <cstdio> 
 
 using namespace std;
+
+int num_case = 0;
+int n;
+int ringPrime_candidate[20];
+bool num_used[20];
 
 int prime_list[] = {2,3,5,7,11,13,17,19,23,29,31};
 
@@ -14,43 +17,37 @@ bool isPrime(int num){
 	return false;
 }
 
-int main(){
-	int num = 0;
-	int num_case = 0;
-	while(scanf("%d", &num) != EOF){
-		vector<int> range_num;
-		for(int i=1; i<=num; ++i) range_num.push_back(i);	// generate vector
-		//for(auto element : range_num) printf("%d ", element); // print			
-		//printf("\n");
-		// Generate Permutations
-		int wasPrintedCase = 0;
-		do {
-			int ring_prime = 1;
-			if(range_num[0] != 1) break;
-			if(!isPrime(range_num[0]+range_num[range_num.size()-1])) continue;
-			for(int i=0; i<range_num.size()-1; ++i){
-				if(!isPrime(range_num[i]+range_num[i+1])){
-					ring_prime = 0;
-					break;
-				}
-			}
-			//if(ring_prime) list_ring_prime.push_back(range_num);
-			if(ring_prime){
-				if(!wasPrintedCase){
-					num_case++;
-					printf("Case %d:\n",num_case);
-					wasPrintedCase = 1;
-				}
-				for(int i=0; i<range_num.size(); ++i){
-		  			if(i == range_num.size()-1) printf("%d", range_num[i]);
-		  			else printf("%d ", range_num[i]);
-	  			}
-	  			printf("\n");
-			}
-	  	} while ( std::next_permutation(range_num.begin(),range_num.end()) );
-	  	printf("\n");
+void printRingPrime(){
+	for(int i = 0; i < n; i++){
+		printf(i == n - 1? "%d" : "%d ", ringPrime_candidate[i]);
 	}
-	// 
-	
+	printf("\n");
+}
+
+void generateRingPrime(int last_valid_index) {
+	// if is the last element of the ring....check if head + tail is prime -> and print the ringPrime_candidate
+	if(last_valid_index == n - 1 && isPrime(ringPrime_candidate[n - 1] + ringPrime_candidate[n])) {
+		printRingPrime();
+		return;
+	}
+	for(int i = 2; i <= n; i++){
+		if(!num_used[i] && isPrime(ringPrime_candidate[last_valid_index] + i)) {
+			num_used[i] = true;
+			ringPrime_candidate[last_valid_index + 1] = i;
+			generateRingPrime(last_valid_index + 1);
+			num_used[i] = false;
+		}
+	}
+}
+
+
+int main(){
+	while(scanf("%d", &n) == 1){
+		num_case++;
+		if(num_case > 1) printf("\n");
+		ringPrime_candidate[0] = ringPrime_candidate[n] = 1;  // first (head) has value '1'...Head is on index '0' and index 'n'
+		printf("Case %d:\n", num_case);
+		generateRingPrime(0);
+	}
 	return 0;
 }
